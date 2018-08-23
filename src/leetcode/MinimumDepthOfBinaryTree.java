79344362c1d178aeea80b8dff221067ecb42acba
ygsama <ygsama@qq.com>
2018-08-23 16:02:52 +0800

    ok

---
+++ src/leetcode/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+import javax.swing.tree.TreeNode;
+
+public class MinimumDepthOfBinaryTree {
+	
+	public int run(TreeNode root) {
+        
+		
+		
+        return 1;
+    }
+}
