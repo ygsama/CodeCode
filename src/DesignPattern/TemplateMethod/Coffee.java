35bbc2531db0fa8bf8ada1b3243d07b3351b663a
ygsama <ygsama@qq.com>
2018-08-30 22:10:14 +0800

    模板方法模式

--- src/DesignPattern/TemplateMethod/Coffee.java
+++ src/DesignPattern/TemplateMethod/Coffee.java
@@ -1,10 +1,14 @@
 package DesignPattern.TemplateMethod;
 
-public class Coffee extends CaffeineBeverage {
+public class Coffee extends CaffeineBeverage implements Comparable {
 	public void brew() {
 		System.out.println("Dripping Coffee through filter");
 	}
 	public void addCondiments() {
 		System.out.println("Adding Sugar and Milk");
+	}
+	@Override
+	public int compareTo(Object o) {
+		return 0;
 	}
 }
--- src/DesignPattern/TemplateMethod/_Test.java
+++ src/DesignPattern/TemplateMethod/_Test.java
@@ -1,17 +1,21 @@
 package DesignPattern.TemplateMethod;
 
+import java.util.Arrays;
+
 /**
  * 模板方法模式:
  * 		定义了一个算法的骨架步骤，将一些步骤延迟到子类中实现。
  * 		使子类在不改变算法结构的情况下，重新定义算法中某些步骤
- * 
+ * 		如Arrays.sort，传入一个实现Comparable接口的对象列表时会调用对象自己的比较算法
  * @author ygsama
  *
  */
 public class _Test {
 
 	public static void main(String[] args) {
-		 
+		Arrays.sort(new int[]{1,2,3});
+		Arrays.sort(new Coffee[]{});
+		
 		Tea tea = new Tea();
 		Coffee coffee = new Coffee();
  
